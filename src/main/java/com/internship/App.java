5cd43923e82dbeb577645074e01c7a1b692bbc65
mouryaaditya20 <aadi29mourya@gmail.com>
2026-02-16 00:05:31 +0530

    Initial Maven Build Automation Project

---
+++ src/main/java/com/internship/App.java
@@ -0,0 +1,10 @@
+package com.internship;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    public static void main(String[] args) {
+        String name = "aaditya";
+        System.out.println("Capitalized Name: " + StringUtils.capitalize(name));
+    }
+}
